48d2c0a37945278861796cd235998d7add16c3bc
Wale Fawehinmi <wale@betaquick.com>
2018-12-26 00:19:35 -0500

    Fix unit tests

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,15 +1,53 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 
 describe('AuthGuard', () => {
+  let authServiceSpy;
+  let authGuard;
+  let routerSpy;
+  const url = '/url';
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [
+        AuthGuard,
+        {provide: AuthService, useValue: jasmine.createSpyObj('authService', ['register', 'login'])},
+        {provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate'])}
+      ]
     });
+    authGuard = TestBed.get(AuthGuard);
+    authServiceSpy = TestBed.get(AuthService);
+    routerSpy = TestBed.get(Router);
   });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+  it('should create auth.guard', () => {
+    expect(authGuard).toBeTruthy();
+  });
+
+  it('checkLogin should return true if user is logged in', () => {
+    authServiceSpy.isLoggedIn = true;
+    expect(authGuard.checkLogin(url)).toEqual(true);
+    expect(routerSpy.navigate.calls.count()).toEqual(0);
+  });
+
+  it('checkLogin should return true if user is logged in - page is redirected', () => {
+    authServiceSpy.isLoggedIn = false;
+    expect(authGuard.checkLogin(url)).toEqual(false);
+    expect(routerSpy.navigate.calls.count()).toEqual(1);
+  });
+
+  it('canActivate should return true if user is logged in', () => {
+    authServiceSpy.isLoggedIn = true;
+    expect(authGuard.canActivate(null, {url})).toEqual(true);
+    expect(routerSpy.navigate.calls.count()).toEqual(0);
+  });
+
+  it('canActivateChild should return true if user is logged in', () => {
+    authServiceSpy.isLoggedIn = true;
+    expect(authGuard.canActivate(null, {url})).toEqual(true);
+    expect(routerSpy.navigate.calls.count()).toEqual(0);
+  });
 });
--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,15 +1,60 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { PhoneTypes } from '@betaquick/grace-tree-constants';
 
 import { AuthService } from './auth.service';
+import { RegisterUser } from '../shared/models/user-model';
 
-describe('AuthService', () => {
+fdescribe('AuthService', () => {
+  let httpClient;
+  let httpTestingController;
+  let authService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       providers: [AuthService]
     });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    authService = TestBed.get(AuthService);
   });
 
-  it('should be created', inject([AuthService], (service: AuthService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('authService should be created', () => {
+    expect(authService).toBeTruthy();
+  });
+
+  describe('register a new user', () => {
+    let user: RegisterUser;
+    const response = {body: {token: 'TOKEN', user: {userId: 1}}};
+
+    beforeEach(() => {
+      user = new RegisterUser();
+      user.firstName = 'Test';
+      user.lastName = 'User';
+      user.password = '1q2w3e4r5t';
+      user.confirmPassword = '1q2w3e4r5t';
+      user.emails = [{
+        emailAddress: 'test@email.com',
+        primary: true
+      }];
+      user.phones = [{
+        phoneNumber: '1234567890',
+        primary: true,
+        phoneType: PhoneTypes.MOBILE
+      }];
+    });
+
+    xit('register a new user - set token and user', () => {
+      authService.register(user)
+        .subscribe(
+          (data) => expect(data).toEqual(response)
+        );
+      const req = httpTestingController.expectOne('/auth/login');
+      req.flush(response);
+      // Finally, assert that there are no outstanding requests.
+      httpTestingController.verify();
+    });
+  });
 });
