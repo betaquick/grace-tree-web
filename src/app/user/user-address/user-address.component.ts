492a986e9813b502b809751dabbacbe06bcbda13
Damilola Animashaun <damilola@sure.group>
2019-02-02 14:46:29 +0100

    Complete User Adress Component

--- src/app/user/user-address/user-address.component.ts
+++ src/app/user/user-address/user-address.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../user.service';
+
 import * as _ from 'lodash';
+import { RegisterUser } from '../../shared/models/user-model';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile-address',
@@ -11,10 +16,59 @@ import * as _ from 'lodash';
 })
 export class UserAddressComponent implements OnInit {
 
+  newAddress: any;
   editMode: boolean;
+  errorMessage: string;
+  address: any;
+  loading: boolean;
 
-  ngOnInit(){
-    this.editMode = false;
-    
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+  ) {
+    this.address = {
+      city: '',
+      street: '',
+      state: '',
+      zip: '',
+      deliveryInstruction: ''
+    };
+
+    this.newAddress = {};
   }
+
+  ngOnInit() {
+    this.editMode = false;
+    this.loading = false;
+
+    this.userService.getUserAddress()
+      .subscribe(addy => {
+        this.address = addy;
+        this.newAddress = Object.assign({}, addy);
+      }, err => this.toastr.error(err));
+
+  }
+
+
+  updateAddress() {
+    this.loading = true;
+    const { city, street, state, zip, deliveryInstruction } = this.newAddress;
+    const newAddress = { city, street, state, zip, deliveryInstruction };
+    this.userService.updateUserAddress(newAddress)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe(
+        (address) => {
+          this.toastr.success('Address updated successfully');
+          this.address = address;
+          this.newAddress = Object.assign({}, address);
+          this.editMode = false;
+          this.loading = false;
+        },
+        err => {
+          this.toastr.error(err);
+          this.loading = false;
+        }
+      );
+  }
+
 }
