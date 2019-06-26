637340b8e1854d5e649eeb0fca077ff15de1dfef
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-06-26 11:45:22 +0100

    Guard company pages form User

---
+++ src/app/auth/role.guard.ts
@@ -0,0 +1,27 @@
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { UserTypes } from '@betaquick/grace-tree-constants';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+
+import { AuthService } from './auth.service';
+
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const roles = route.data['roles'] as UserTypes[];
+    const { user } = this.authService;
+
+    if (roles.indexOf(user.userType) < 0) {
+      this.router.navigate(['/']);
+
+      return false;
+    }
+
+    return true;
+  }
+}
