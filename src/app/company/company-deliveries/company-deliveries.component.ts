4054a2aa998e3088404676c764d7edf3701facb1
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-07-01 21:49:05 +0100

    Update view delivery template

--- src/app/company/company-deliveries/company-deliveries.component.ts
+++ src/app/company/company-deliveries/company-deliveries.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { finalize } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { SessionStorage } from 'ngx-store';
 
 import { CompanyService } from '../company.service';
+import { User } from '../../shared/models/user-model';
 
 @Component({
   selector: 'app-company-deliveries',
   templateUrl: './company-deliveries.component.html',
   styleUrls: ['./company-deliveries.component.scss']
 })
-export class CompanyDeliveriesComponent implements OnInit {
+export class CompanyDeliveriesComponent implements OnInit, OnDestroy {
 
+  @SessionStorage() user: User = new User();
   loading: boolean;
 
   deliveries = [];
   delivery: any = {};
+  crews = [];
 
   constructor(
     private router: Router,
@@ -25,6 +29,7 @@ export class CompanyDeliveriesComponent implements OnInit {
 
   ngOnInit() {
     this.getDeliveries();
+    this.getCompanyCrews();
   }
 
   getDeliveries() {
@@ -35,6 +40,25 @@ export class CompanyDeliveriesComponent implements OnInit {
       .subscribe(deliveries => this.deliveries = deliveries,
         err => this.toastr.error(err)
       );
+  }
+
+  getCompanyCrews() {
+    this.loading = true;
+
+    this.companyService
+      .getCompanyCrews()
+      .pipe(finalize(() => this.loading = false))
+      .subscribe(crews => this.crews = crews,
+      err => this.toastr.error(err)
+    );
+  }
+
+  getCrewMemberName(id: number) {
+    const crewMember = [...this.crews, this.user].filter(c => c.userId === id)[0];
+    if (crewMember) {
+      return `${crewMember.firstName} ${crewMember.lastName}`;
+    }
+    return 'Unknown Crew Member';
   }
 
   openModal(modal: any, data) {
@@ -50,4 +74,6 @@ export class CompanyDeliveriesComponent implements OnInit {
 
     modal.show();
   }
+
+  ngOnDestroy(): void {}
 }
