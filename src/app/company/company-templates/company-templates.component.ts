1efec2e6f6cfbc4d8e2c3500b447d3aae647ee75
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-09-27 00:26:28 +0100

    group templates

--- src/app/company/company-templates/company-templates.component.ts
+++ src/app/company/company-templates/company-templates.component.ts
@@ -4,6 +4,7 @@ import { ToastrService } from 'ngx-toastr';
 
 import { Template } from '../../shared/models/template-model';
 import { CompanyService } from '../company.service';
+import { NotificationTypes } from '@betaquick/grace-tree-constants';
 
 
 @Component({
@@ -18,6 +19,7 @@ export class CompanyTemplateComponent implements OnInit {
 
   loading: boolean;
   templates: Template[];
+  grouped_templates: Template[][] = [];
 
   constructor(
     private companyService: CompanyService,
@@ -37,8 +39,50 @@ export class CompanyTemplateComponent implements OnInit {
     this.companyService
       .getTemplates()
       .pipe(finalize(() => this.loading = false))
-      .subscribe(templates => this.templates = templates,
+      .subscribe(templates => {
+        this.groupTemplates(templates);
+        this.templates = templates;
+      },
       err => this.toastr.error(err)
     );
+  }
+
+  groupTemplates(templates: Template[]) {
+    this.grouped_templates = [
+      [
+        templates.find(t => t.notificationType === NotificationTypes.CompanyDeliveryEmail),
+        templates.find(t => t.notificationType === NotificationTypes.CompanyDeliveryEmail),
+      ],
+
+      [
+        templates.find(t => t.notificationType === NotificationTypes.CrewRegistrationEmail),
+        templates.find(t => t.notificationType === 'CREW REGISTRATION SMS'),
+      ],
+
+      [
+        templates.find(t => t.notificationType === NotificationTypes.CompanyDeliveryRequestEmail),
+        templates.find(t => t.notificationType === NotificationTypes.DeliveryRequestAcceptanceEmail),
+      ],
+
+      [
+        templates.find(t => t.notificationType === NotificationTypes.DeliveryRequestSMS),
+        templates.find(t => t.notificationType === NotificationTypes.DeliveryRequestAcceptanceSMS),
+      ],
+
+      [
+        templates.find(t => t.notificationType === NotificationTypes.UserRegistrationAdminEmail),
+        templates.find(t => t.notificationType === 'NOTIFY ADMIN OF USER REGISTRATION SMS'),
+      ],
+
+      [
+        templates.find(t => t.notificationType === NotificationTypes.UserStatusEmail),
+        templates.find(t => t.notificationType === NotificationTypes.UserDeliveryEmail),
+        templates.find(t => t.notificationType === NotificationTypes.DeliveryWarningEmail),
+        templates.find(t => t.notificationType === 'USER STATUS UPDATE SMS'),
+        templates.find(t => t.notificationType === NotificationTypes.UserDeliverySMS),
+        templates.find(t => t.notificationType === NotificationTypes.DeliveryWarningSMS),
+      ]
+
+    ];
   }
 }
