282b0f0eb39e20f604ec7e3ba1f041858f3e3fce
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-10-12 21:38:56 +0100

    Handle 401s

--- src/app/http-interceptor/index.ts
+++ src/app/http-interceptor/index.ts
@@ -1,8 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AuthInterceptor } from './auth-interceptor';
+import { AuthErrorInterceptor } from './auth-error-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthErrorInterceptor, multi: true }
 ];
