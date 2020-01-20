0f8db249a09fcc7a9ad430352d016245c57da4dd
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-20 20:31:52 +0100

    Update Terms & Conditions

--- src/app/auth/user-registration/agreement/agreement.component.ts
+++ src/app/auth/user-registration/agreement/agreement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { finalize, flatMap } from 'rxjs/operators';
@@ -13,11 +13,37 @@ import { AuthService } from '../../auth.service';
   styleUrls: [
     './agreement.component.scss',
     '../../../../assets/icon/icofont/css/icofont.scss'
+  ],
+  styles: [
+    ` .wrong__name { border-color: #dc3545 !important; } `
   ]
 })
 export class AgreementComponent implements OnInit {
+  @ViewChildren('term') termsCheckboxes: QueryList<{ nativeElement: HTMLInputElement }>;
   loading: boolean;
-  acceptAcceptment: boolean;
+  clientName: string;
+  expectedName: string;
+
+  get allTermsAgreed(): boolean {
+    if (this.termsCheckboxes) {
+      return this.termsCheckboxes.toArray().every(input => input.nativeElement.checked);
+    }
+    return false;
+  }
+
+  get correctName(): boolean {
+    const { user } = this.authService;
+    return (this.clientName || '').toLowerCase() === `${user.firstName} ${user.lastName}`.toLowerCase();
+  }
+
+  terms = [
+    'I am responsible to maintain my profile up to date with all of my current information.',
+    'I am responsible to maintain my correct status for a delivery - “Ready” or “Pause”',
+    'I am responsible to mark all attempted deliveries as successful or unsuccessful on my account',
+    // tslint:disable-next-line: max-line-length
+    '*ChipDump Arborist Products & Services (AKA gracetreeservices.com), is a free, information management service designed to put people that want Arborist & other products in touch with people and/or companies (Mostly Arborists & Landscapers), that have those products to give away. ChipDump has no employees and has no authority over the Arborists/Landscapers and others that participate in the program. They subscribe to the program just like you do. Should any issues or problems arise or result regarding any aspect of a delivery, those should be resolved between the recipient and the delivering party/Arborist/Landscaper or whoever (company or driver), that is making the delivery. ',
+    'I have read, understand and agree with all of the Terms and Conditions of the ChipDump program, signified by my checking of each one.'
+  ];
 
   constructor(
     private authService: AuthService,
@@ -27,11 +53,11 @@ export class AgreementComponent implements OnInit {
 
   ngOnInit() {
     this.loading = false;
-    this.acceptAcceptment = false;
+    this.expectedName = `${this.authService.user.firstName} ${this.authService.user.lastName}`;
   }
 
   acceptAgreement() {
-    if (this.loading || !this.acceptAcceptment) {
+    if (this.loading || !this.correctName || !this.allTermsAgreed) {
       return;
     }
     this.loading = true;
